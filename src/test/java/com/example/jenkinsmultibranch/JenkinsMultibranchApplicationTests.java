3d1ad8859731036a927999f30634b616a87dee85
hamid <hamid.shafikani@gmail.com>
2020-07-08 21:12:29 +0200

    Initial commit

---
+++ src/test/java/com/example/jenkinsmultibranch/JenkinsMultibranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsmultibranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMultibranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
